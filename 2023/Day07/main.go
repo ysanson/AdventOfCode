0a1fbe341a9a8cd76f4319b3814862ec67fb03bf
Yvan SANSON <sansonyvan@gmail.com>
2023-12-07 17:07:54 +0100

    Day 7 done

--- 2023/Day07/main.go
+++ 2023/Day07/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"slices"
 	"strings"
 
@@ -25,69 +24,118 @@ type Hand struct {
 	strength int
 }
 
-var cards = []rune{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
+var cardsPart1 = []rune{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
+var cardsPart2 = []rune{'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'}
 
-func getHandStrengh(hand string) int {
-	cardOccurences := make(map[rune]int, len(cards))
-	for _, card := range cards {
+func getHandStrengh(hand string, includeJoker bool) int {
+	cardOccurences := make(map[rune]int, len(cardsPart1))
+	nbJokers := strings.Count(hand, "J")
+	for _, card := range cardsPart1 {
 		cardOccurences[card] = strings.Count(hand, string(card))
 	}
 	if pkg.MapContains(cardOccurences, 5) {
 		return FiveKind
 	} else if pkg.MapContains(cardOccurences, 4) {
+		if includeJoker && nbJokers > 0 {
+			return FiveKind
+		}
 		return FourKind
 	} else if pkg.MapContains(cardOccurences, 3) && pkg.MapContains(cardOccurences, 2) {
-		return ThreeKind
-	} else if pkg.MapContains(cardOccurences, 3) && !pkg.MapContains(cardOccurences, 2) {
+		if includeJoker && nbJokers > 0 {
+			return FiveKind
+		}
 		return FullHouse
+	} else if pkg.MapContains(cardOccurences, 3) && !pkg.MapContains(cardOccurences, 2) {
+		if includeJoker && nbJokers == 1 {
+			return FourKind
+		}
+		return ThreeKind
 	} else if pkg.MapCountOccurrences(cardOccurences, 2) == 2 {
+		if includeJoker {
+			switch nbJokers {
+			case 1:
+				return FullHouse
+			case 2:
+				return FourKind
+			default:
+				return TwoPair
+			}
+		}
 		return TwoPair
 	} else if pkg.MapCountOccurrences(cardOccurences, 2) == 1 {
+		if includeJoker && nbJokers > 0 {
+			return ThreeKind
+		}
 		return OnePair
 	} else {
+		if includeJoker && nbJokers > 0 {
+			return OnePair
+		}
 		return HighCard
 	}
 }
 
-func convertHandToNumber(hand string) int {
-	var num strings.Builder
-	for _, card := range hand {
-		num.WriteString(fmt.Sprint(slices.Index(cards, card)))
+func getCommonPrefixLength(hand1, hand2 string) int {
+	for i := 0; i < len(hand1); i++ {
+		if hand1[i] != hand2[i] {
+			return i
+		}
 	}
-	str := num.String()
-	return pkg.MustAtoi(str)
+	return len(hand1)
 }
 
 func run(input string) (interface{}, interface{}) {
 	lines := strings.Split(input, "\n")
-	hands := make([]Hand, len(lines))
+	handsPart1 := make([]Hand, len(lines))
 	var split []string
-	var h string
 	for idx, line := range lines {
 		split = strings.Split(line, " ")
-		h = split[0]
-		hands[idx] = Hand{
-			hand:     h,
+		handsPart1[idx] = Hand{
+			hand:     split[0],
 			bid:      pkg.MustAtoi(split[1]),
-			strength: getHandStrengh(h),
+			strength: getHandStrengh(split[0], false),
 		}
 	}
 
-	slices.SortFunc(hands, func(a, b Hand) int {
+	slices.SortFunc(handsPart1, func(a, b Hand) int {
 		if a.strength != b.strength {
 			return a.strength - b.strength
 		} else {
-			return convertHandToNumber(a.hand) - convertHandToNumber(b.hand)
+			commonPrefix := getCommonPrefixLength(a.hand, b.hand)
+			return slices.Index(cardsPart1, pkg.GetChar(a.hand, commonPrefix)) - slices.Index(cardsPart1, pkg.GetChar(b.hand, commonPrefix))
 		}
 	})
 
 	part1 := 0
 
-	for idx, hand := range hands {
-		part1 += hand.strength * (idx + 1)
+	for idx, hand := range handsPart1 {
+		part1 += hand.bid * (idx + 1)
 	}
 
-	return part1, 0
+	handsPart2 := make([]Hand, len(lines))
+	for idx, line := range lines {
+		split = strings.Split(line, " ")
+		handsPart2[idx] = Hand{
+			hand:     split[0],
+			bid:      pkg.MustAtoi(split[1]),
+			strength: getHandStrengh(split[0], true),
+		}
+	}
+
+	slices.SortFunc(handsPart2, func(a, b Hand) int {
+		if a.strength != b.strength {
+			return a.strength - b.strength
+		} else {
+			commonPrefix := getCommonPrefixLength(a.hand, b.hand)
+			return slices.Index(cardsPart2, pkg.GetChar(a.hand, commonPrefix)) - slices.Index(cardsPart2, pkg.GetChar(b.hand, commonPrefix))
+		}
+	})
+	part2 := 0
+	for idx, hand := range handsPart2 {
+		part2 += hand.bid * (idx + 1)
+	}
+
+	return part1, part2
 }
 
 func main() {
--- 2023/Day07/puzzle.go
+++ 2023/Day07/puzzle.go
@@ -6,7 +6,12 @@ var Tests = execute.TestCases{
 	{
 		Input:         Test1,
 		ExpectedPart1: 6440,
-		ExpectedPart2: 0,
+		ExpectedPart2: 5905,
+	},
+	{
+		Input:         Test2,
+		ExpectedPart1: 1343,
+		ExpectedPart2: 1369,
 	},
 	// {
 	// 	Input:         Puzzle,
@@ -20,6 +25,21 @@ T55J5 684
 KK677 28
 KTJJT 220
 QQQJA 483`
+
+const Test2 = `AAAAA 2
+22222 3
+AAAAK 5
+22223 7
+AAAKK 11
+22233 13
+AAAKQ 17
+22234 19
+AAKKQ 23
+22334 29
+AAKQJ 31
+22345 37
+AKQJT 41
+23456 43`
 
 const Puzzle = `8KKK8 324
 28885 350
