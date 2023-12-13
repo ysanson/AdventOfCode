06e35b3f39af1041eafcf465c114819004412d32
Yvan SANSON <sansonyvan@gmail.com>
2023-12-13 16:02:53 +0100

    Day 12 thanks to https://github.com/ConcurrentCrab/AoC/blob/main/solutions/12-2.go, I don't understand everything

--- 2023/pkg/parse.go
+++ 2023/pkg/parse.go
@@ -36,9 +36,7 @@ func ParseIntList(s, sep string) []int {
 	lines := strings.Split(s, sep)
 	list := make([]int, len(lines))
 	for i, line := range lines {
-		nb, err := strconv.Atoi(line)
-		Check(err)
-		list[i] = nb
+		list[i] = MustAtoi(line)
 	}
 	return list
 }
