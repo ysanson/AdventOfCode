e997ec0a152093d036a82328a0b986a068994e2c
Yvan Sanson <sansonyvan@gmail.com>
2024-12-03 22:36:19 +0900

    Added puzzle template
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>

---
+++ puzzle_example.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/ysanson/AdventOfCode/pkg/execute"
+
+var Tests = execute.TestCases{
+	{
+		Input:         Test1,
+		ExpectedPart1: 0,
+		ExpectedPart2: 0,
+	},
+	{
+		Input:         Test2,
+		ExpectedPart1: 0,
+		ExpectedPart2: 0,
+	},
+	{
+		Input:         Puzzle,
+		ExpectedPart1: 0,
+		ExpectedPart2: 0,
+	},
+}
+
+const Test1 = ``
+
+const Test2 = ""
+
+const Puzzle = ``
