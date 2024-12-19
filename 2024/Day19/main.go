b555e5e4633e08bf748d2f79495887ec4ef1e419
Yvan Sanson <sansonyvan@gmail.com>
2024-12-19 16:28:58 +0900

    Day 19
    Signed-off-by: Yvan Sanson <sansonyvan@gmail.com>

---
+++ 2024/Day19/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/ysanson/AdventOfCode/pkg/execute"
+)
+
+func findPossibleCombinations(towels []string, pattern string, previousPatterns *map[string]int) (combinations int) {
+	if comb, ok := (*previousPatterns)[pattern]; ok {
+		return comb
+	}
+	if len(pattern) == 0 {
+		combinations = 1
+	} else {
+		for _, towel := range towels {
+			if after, found := strings.CutPrefix(pattern, towel); found {
+				if comb := findPossibleCombinations(towels, after, previousPatterns); comb > 0 {
+					combinations += comb
+					(*previousPatterns)[after] = comb
+				}
+			}
+		}
+	}
+	return
+}
+
+func run(input string) (interface{}, interface{}) {
+	part1, part2 := 0, 0
+	lines := strings.Split(input, "\n")
+	t, patterns := strings.Split(lines[0], ","), lines[2:]
+	towels := make([]string, len(t))
+	for i, towel := range t {
+		towels[i] = strings.TrimPrefix(towel, " ")
+	}
+	previousPatterns := make(map[string]int)
+	for _, pattern := range patterns {
+		if count := findPossibleCombinations(towels, pattern, &previousPatterns); count > 0 {
+			part1++
+			part2 += count
+		}
+	}
+
+	return part1, part2
+}
+
+func main() {
+	execute.Run(run, Tests, Puzzle, false)
+}
